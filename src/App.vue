09014cdce565742a6602d1d3059d2ecaa32c218c
shanshun <shanshun@raybattle.com>
2019-11-13 15:24:19 +0800

    index页面

--- src/App.vue
+++ src/App.vue
@@ -55,40 +55,80 @@
     /* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
     @import './common/uni.css';
 
-    /* 以下样式用于 hello uni-app 演示所需 */
-    page {
-        background-color: #F4F5F6;
-        height: 100%;
-        font-size: 28upx;
-        line-height: 1.8;
+    /*每个页面公共css */
+    *{
+        margin:0;
+        padding:0;
+        list-style: none;
     }
 
-    .uni-header-logo {
-        padding: 30upx;
+    .content-body {
+        width: 100vw;
+        min-height: 100vh;
+        background: #fff6db;
+        display: flex;
         flex-direction: column;
-        justify-content: center;
         align-items: center;
-        margin-top: 10upx;
+        position: relative;
+    }
+    .wave {
+        position: relative;
+        overflow: hidden;
+    }
+    .wave:after {
+        content: "";
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        pointer-events: none;
+        background-image: radial-gradient(circle, #666 10%, transparent 10.01%);
+        background-repeat: no-repeat;
+        background-position: 50%;
+        transform: scale(10, 10);
+        opacity: 0;
+        transition: transform .3s, opacity .5s;
+    }
+    .wave:active:after {
+        transform: scale(0, 0);
+        opacity: .3;
+        transition: 0s;
+    }
+    .wave:active {
+        opacity: .8;
     }
 
-    .uni-header-image {
-        width: 100px;
-        height: 100px;
+
+    .logo_bottom {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        height: 350rpx;
     }
 
-    .uni-hello-text {
-        color: #7A7E83;
+    /*模态框弹出窗口的动画*/
+    @keyframes moveUpAnimate {
+        0% {
+            transform: translateY(100%);
+        }
+        100% {
+            transform: translateY(0);
+        }
     }
-
-    .uni-hello-addfile {
-        text-align: center;
-        line-height: 300upx;
-        background: #FFF;
-        padding: 50upx;
-        margin-top: 10px;
-        font-size: 38upx;
-        color: #808080;
+    .moveUpAnimate {
+        animation: moveUpAnimate .5s;
     }
-
-    /* #endif*/
+    @keyframes fadeInAnimate {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+    .fadeInAnimate {
+        animation: fadeInAnimate 0.5s;
+    }
 </style>
--- src/pages/tabBar/index/index.vue
+++ src/pages/tabBar/index/index.vue
@@ -1,13 +1,75 @@
 <template>
-    <div>我是index11111111</div>
+    <div class="content-body">
+        <div class="uni-padding-wrap box">
+            <div class="page-section swiper">
+                <div class="page-section-spacing">
+                    <swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval"
+                            :duration="duration">
+                        <swiper-item>
+                            <!--<view class="swiper-item uni-bg-red">A</view>-->
+                            <image mode="widthFix" class="banner" src="../../../static/food/banner.jpg"></image>
+                        </swiper-item>
+
+                        <swiper-item>
+                            <image mode="widthFix" class="banner" src="../../../static/food/banner1.jpg"></image>
+                        </swiper-item>
+
+                    </swiper>
+                </div>
+            </div>
+        </div>
+        <div class="searchBox">
+            <input class="input" name="" placeholder="请输入您要搜索的美食"/>
+            <icon type="search" size="20" class="btn"/>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "index"
+        // name: "index"
+        data() {
+            return {
+                autoplay: true,
+                interval: 2500,
+                duration: 500,
+                indicatorDots: true,
+            }
+        },
+        methods: {}
     }
 </script>
 
 <style scoped>
+    .box {
+        width: 100%;
+        /*height: 700px;*/
+    }
 
+    .banner {
+        width: 100%;
+        height: 300px;
+    }
+
+    .searchBox {
+        width: 70%;
+        /*height: 50px;*/
+        padding-bottom: 3px;
+        background: white;
+        margin-top: 15px;
+        display: flex;
+        align-items: center;
+    }
+
+    .input {
+        width: 70%;
+        height: 30px;
+        border:none;
+        outline: none;
+        padding-left: 8px;
+        font-size: 15px;
+    }
+    .btn{
+        margin-left: 10%;
+    }
 </style>
